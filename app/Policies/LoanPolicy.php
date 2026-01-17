a7893d3f5559d298c59f9ac3f87498a6df7824c5
ahprinters <ahps323@gmail.com>
2026-01-17 18:43:23 +0600

    policy component

--- app/Policies/LoanPolicy.php
+++ app/Policies/LoanPolicy.php
@@ -3,14 +3,30 @@
 namespace App\Policies;
 
 use App\Models\User;
+use App\Models\Loan;
+use Illuminate\Auth\Access\HandlesAuthorization;
 
 class LoanPolicy
 {
-    /**
+
+use HandlesAuthorization;
+/**
      * Create a new policy instance.
      */
-    public function __construct()
+    public function viewAny(User $user)
     {
-        //
+        return true; // Authenticated users can view their loans
+
+    }
+
+    public function view( User $user, Loan $loan)
+    {
+        return $user->id === $loan->user_id;// user can update only their
+    }
+
+    public function delete(User $user, Loan $loan)
+    {
+        return $user->id === $loan->user_id; // User can delete only their
+
     }
 }
